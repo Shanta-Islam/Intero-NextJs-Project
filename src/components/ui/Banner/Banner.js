4e4472610ed5698438aced4608180f921a649474
Shanta-Islam <shantaislamshorna68@gmail.com>
2024-02-02 17:01:00 +0600

    'updte'

---
+++ src/components/ui/Banner/Banner.js
@@ -0,0 +1,14 @@
+
+const Banner = () => {
+    return (
+        <div className="bg-gray-900 text-white py-20">
+            <div className="container mx-auto">
+                <h1 className="text-4xl font-bold">Best Dev</h1>
+                <p className="text-lg mt-4">Allow me to show you my awesome work!</p>
+                <button className="bg-blue-600 text-white rounded-full px-6 py-2 mt-8 hover:bg-blue-500">Learn More</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
